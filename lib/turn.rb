6469d655e52e3b10a172c21f70f6f30a4159c4eb
DownsideupmediaUSA <brandondecarlo@live.com>
2016-01-26 21:28:52 +0000

    nitrous-backup at Tue Jan 26 21:28:52 UTC 2016

---
+++ lib/turn.rb
@@ -0,0 +1,22 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+
+def position_taken?(board, position, character = "X")
+  board[position.to_i] = character
+  end
+
+def valid_move?(board,position)
+  if position.to_i-1 == 1..9
+    false
+  elsif board[position.to_i-1] == " "
+   true
+  end
+
+
+
+end
